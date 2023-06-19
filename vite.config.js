489e9288ee72cdfc02030455e23577b8d524b85a
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-06-19 18:42:05 +0200

    [FIX] Port and build command

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3042,
+  },
   plugins: [
-    vue(),
+    vue({
+        template: {
+            transformAssetUrls: {
+                includeAbsolute: false,
+            },
+        },
+    }),
   ],
   resolve: {
     alias: {
