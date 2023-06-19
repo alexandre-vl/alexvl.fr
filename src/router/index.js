417478917d4289b2fc142b266444ed7efe71ee40
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-06-20 01:03:21 +0200

    [FIX] Clean code + starting adding psswd generator

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const router = createRouter({
                     component: () => import('../views/Home.vue')
                 }
             ] */
+        },
+        {
+            path: '/',
+            name: 'password',
+            component: () => import('../views/tools/Password.vue'),
         }
     ]
 })
---
+++ src/views/tools/Password.vue
@@ -0,0 +1,7 @@
+<template>
+    <main>
+        <div>
+
+        </div>
+    </main>
+</template>
